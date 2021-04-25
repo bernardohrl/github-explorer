80b0cb31ed057ff1f8267a7c9905bf4fb7b7ad77
Bernardo Henrique <bernardohrl.unb@gmail.com>
2021-04-24 21:56:25 -0300

    Starts configuration
    - Adds Angular Material
    - Creates views explorer and details
    - Configs Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExplorerComponent } from './views/explorer/explorer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ExplorerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
