92920e3810bba12fc33370149996e448a53e57a6
Bernardo Henrique <bernardohrl.unb@gmail.com>
2021-04-25 13:05:30 -0300

    Creates ReposList
    Adds Repositories List and Flex Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ExplorerComponent } from './views/explorer/explorer.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { DetailsComponent } from './views/details/details.component';
+import { ExplorerComponent } from './views/explorer/explorer.component';
+import { ReposListComponent } from './views/explorer/repos-list/repos-list.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card'; 
+import { FlexLayoutModule } from '@angular/flex-layout'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     ExplorerComponent,
-    DetailsComponent
+    DetailsComponent,
+    ReposListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
